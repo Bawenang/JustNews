a2e1db67ea3b2689008c9c89a0082fe8e311ca14
Bawenang R. P. P. <bawenang@gmail.com>
2024-11-14 09:55:12 +0700

    Add Get News use case

---
+++ JustNews/Contexts/News/Domain/UseCases/GetNews.swift
@@ -0,0 +1,26 @@
+//
+//  GetNews.swift
+//  JustNews
+//
+//  Created by Bawenang RPP on 14/11/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol GetNewsUseCase {
+    func retrieveAll() -> Observable<[NewsItem]>
+}
+
+class GetNews: GetNewsUseCase {
+    func retrieveAll() -> RxSwift.Observable<[NewsItem]> {
+        let mock = NewsItem(
+            id: 1,
+            title: "Test",
+            content: "Teststststa",
+            thumbnail: URL(string: "https://dummyimage.com/200x200/753b01/suspendisse.png&text=jsonplaceholder.org"),
+            image: URL(string: "https://dummyimage.com/800x430/753b01/suspendisse.png&text=jsonplaceholder.org")
+        )
+        return .just([mock])
+    }
+}
