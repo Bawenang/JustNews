fa1c412b5875d595c52bb89e4d14121efbd8e40d
Bawenang R. P. P. <bawenang@gmail.com>
2024-11-14 09:42:49 +0700

    Add NewsItem entity

---
+++ JustNews/Contexts/News/Domain/Entities/NewsItem.swift
@@ -0,0 +1,16 @@
+//
+//  NewsItem.swift
+//  JustNews
+//
+//  Created by Bawenang RPP on 14/11/24.
+//
+
+import Foundation
+
+struct NewsItem {
+    let id: Int
+    let title: String
+    let content: String
+    let thumbnail: URL?
+    let image: URL?
+}
