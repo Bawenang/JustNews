c5e6d404383f02aff1df0deabf63db005dbc9a7b
Bawenang R. P. P. <bawenang@gmail.com>
2024-11-14 09:12:02 +0700

    Initial commit

---
+++ JustNewsTests/JustNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustNewsTests.swift
+//  JustNewsTests
+//
+//  Created by Bawenang RPP on 14/11/24.
+//
+
+import Testing
+@testable import JustNews
+
+struct JustNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
